87b300f8051fa431fddc90d949f4fa591ecf078f
fakestarbaby <fakestarbaby@gmail.com>
2012-10-09 23:37:21 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleQiita::Application.config.secret_token = 'fdae56c9d1855e280a88810107589a2ac61020c600c741e3ef908d28a626d406daf6d59c0853c65a6f056f2c417c3d25529e5c8c6d79eaa2b6ae979a60df7345'
